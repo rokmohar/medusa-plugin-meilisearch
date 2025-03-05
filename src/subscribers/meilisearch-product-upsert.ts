9f3178db557ec07147a4aeddeae0fed5e65c7881
kb-0912 <78142541+kb-0912@users.noreply.github.com>
2025-03-05 21:29:11 +0700

    verify product status before add document

--- src/subscribers/meilisearch-product-upsert.ts
+++ src/subscribers/meilisearch-product-upsert.ts
@@ -12,7 +12,14 @@ export default async function meilisearchProductUpsertHandler({
   const meilisearchService: MeiliSearchService = container.resolve('meilisearch')
 
   const product = await productModuleService.retrieveProduct(productId)
-  await meilisearchService.addDocuments('products', [product], SearchUtils.indexTypes.PRODUCTS)
+
+  if (product.status === 'published') {
+    // If status is "published", add or update the document in MeiliSearch
+    await meilisearchService.addDocuments('products', [product], SearchUtils.indexTypes.PRODUCTS)
+  } else {
+    // If status is not "published", remove the document from MeiliSearch
+    await meilisearchService.deleteDocument('products', productId)
+  }
 }
 
 export const config: SubscriberConfig = {
